dc7cf5bf097b35967cf3d2a76ea991339160fdef
Aibarret <83474314+ZplaxAgogNonZeep@users.noreply.github.com>
2022-07-15 21:12:37 -0400

    Enemy Pathing Improved and Added Enemy Tween

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -7,10 +7,16 @@ var dir = 1
 
 func new_turn():
 	yield(get_tree(), "idle_frame")
-	$RayCast2D.cast_to = Vector2(15 * dir, 0)
+	$RayCast2D.cast_to = Vector2(16 * dir, 0)
 	$RayCast2D.force_raycast_update()
 	if $RayCast2D.is_colliding():
 		dir *= -1
-		new_turn()
-	else:
-		position += Vector2(get_parent().game.TILE_SIZE * dir, 0)
+		$RayCast2D.cast_to = Vector2(16 * dir, 0)
+		$RayCast2D.force_raycast_update()
+		if $RayCast2D.is_colliding():
+			dir = 0
+	tween_to(position + Vector2(get_parent().game.TILE_SIZE * dir, 0))
+
+func tween_to(posn : Vector2):
+	$Tween.interpolate_property(self, "position", position, posn, .1)
+	$Tween.start()
