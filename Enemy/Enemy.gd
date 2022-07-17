f233b72d3fb633e02d31e067f4d0082206a733de
LiviRose <72127278+LiviRoseGames@users.noreply.github.com>
2022-07-16 21:37:20 -0700

    Revert "Audio"

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -37,7 +37,6 @@ func die():
 	queue_free()
 	
 func take_damage(game, dmg):
-	self.game.get_node("AudioManager").startTrack("AttackSFX", 0, false)
 		# TODO: Tweak values
 	health = max(0, health - dmg)
 	$HPBar.value = health
