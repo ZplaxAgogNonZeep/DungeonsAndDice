34624df3f7b3df51ac75fb08efe0cfa857a769e7
Aibarret <83474314+ZplaxAgogNonZeep@users.noreply.github.com>
2022-07-17 05:48:30 -0400

    function can be called to change dice on UI

--- UI/HitDice.gd
+++ UI/HitDice.gd
@@ -1,11 +1,16 @@
 extends Control
 
+
 var previousHealth := 6
 
+
+#func _ready():
+#	changeDice(3)
+
 func changeDice(newHealth : int):
-	if newHealth > previousHealth:
-		pass
-	elif newHealth < previousHealth:
-		pass
-	else:
-		pass
+	for i in range(6):
+			if i <= newHealth:
+				get_node("Dice" + str(i)).texture = load("res://art/Dice" + str(i + 1) +  ".png")
+			else:
+				get_node("Dice" + str(i)).texture = load("res://art/black.png")
+
